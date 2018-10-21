bdad609e34550656b03a3de1267d2f352795f32c
Zach Nusbaum <zach@zdnmail.com>
2018-10-21 10:45:22 -0700

    Add distance calculator

--- src/app/distance-calculator/distance-calculator.component.ts
+++ src/app/distance-calculator/distance-calculator.component.ts
@@ -1,4 +1,6 @@
+import { ZippoService } from './../zippo.service';
 import { Component, OnInit } from '@angular/core';
+import * as turf from '@turf/turf';
 
 @Component({
   selector: 'app-distance-calculator',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./distance-calculator.component.scss']
 })
 export class DistanceCalculatorComponent implements OnInit {
+  distance: number;
 
-  constructor() { }
+  constructor(private zippo: ZippoService) { }
 
   ngOnInit() {
+    // this.calculateDistance('92108', '92111');
+  }
+
+  calculateDistance(zip1, zip2) {
+    this.zippo.parseTwo(zip1, zip2).subscribe((responses: any) => {
+      // let coords1 = {lat: responses[0].places[0].latitude, lng: responses[0].places[0].longitude};
+      let from = turf.point([responses[0].places[0].latitude, responses[0].places[0].longitude]);
+
+      // let coords2 = {lat: responses[1].places[0].latitude, lng: responses[1].places[0].longitude};
+      let to = turf.point([responses[1].places[0].latitude, responses[1].places[0].longitude]);
+
+      this.distance = turf.distance(from, to);
+    });
+    return false;
   }
 
 }
--- src/app/zippo.service.ts
+++ src/app/zippo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,18 @@ export class ZippoService {
 
   constructor(private http: HttpClient) { }
 
-  public parse(zip) {
+  public parse(zip: string) {
     return this.http.get(`https://api.zippopotam.us/us/${zip}`);
   }
 
   public reverse(city: string, state: string) {
     return this.http.get(`https://api.zippopotam.us/us/${state}/${city}`);
+  }
+
+  parseTwo(zip1: string, zip2: string) {
+    let req1 = this.http.get(`https://api.zippopotam.us/us/${zip1}`);
+    let req2 = this.http.get(`https://api.zippopotam.us/us/${zip2}`);
+
+    return forkJoin([req1, req2]);
   }
 }
