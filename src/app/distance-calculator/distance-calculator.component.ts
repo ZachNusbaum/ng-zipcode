ab2fcf447eff38de4f2e570707b7d3772ed5860d
Zach Nusbaum <zach@zdnmail.com>
2018-10-21 10:59:39 -0700

    flipped lat and lng in distance function

--- src/app/distance-calculator/distance-calculator.component.ts
+++ src/app/distance-calculator/distance-calculator.component.ts
@@ -19,10 +19,10 @@ export class DistanceCalculatorComponent implements OnInit {
   calculateDistance(zip1, zip2) {
     this.zippo.parseTwo(zip1, zip2).subscribe((responses: any) => {
       // let coords1 = {lat: responses[0].places[0].latitude, lng: responses[0].places[0].longitude};
-      let from = turf.point([responses[0].places[0].latitude, responses[0].places[0].longitude]);
+      let from = turf.point([responses[0].places[0].longitude, responses[0].places[0].latitude,]);
 
       // let coords2 = {lat: responses[1].places[0].latitude, lng: responses[1].places[0].longitude};
-      let to = turf.point([responses[1].places[0].latitude, responses[1].places[0].longitude]);
+      let to = turf.point([responses[1].places[0].longitude, responses[1].places[0].latitude]);
 
       this.distance = turf.distance(from, to);
     });
